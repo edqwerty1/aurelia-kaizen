80b4b7d13dc2dcad40ddc580030346f3a4f57a20
Jonathan Bass <jonathan.bass@advancedcomputersoftware.com>
2016-03-21 14:25:45 +0000

    Added a simple UI example with: - a repeater - Conditional styles - various HTML form/control events being bound to functions - All written with a simple and testable ts class (refactoring and tests coming soon)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 /// <reference path="../typings/main.d.ts" />
 
 export class App {
-  message: string = 'Welcome to Aurelia';
-     
+  message: string = 'Welcome to Aurelia'; 
+  currentTask:string = "";
+  hasFocus: boolean = true;
+  tasks:string[] = [];
+  colour:string = "black";
+  private colours: string[] = [
+      "red","yellow","pink","green","purple", "black"
+  ]
   public GetMessage() {
       return this.message;
+  }
+  
+  public AddTask()  {
+     this.tasks.push(this.currentTask);
+     this.currentTask = "";
+     this.hasFocus = true;
+     this.colour = this.colours[Math.floor((Math.random()*6))];
+  }
+  
+  public RemoveTask(index)  {
+     this.tasks.splice(index, 1);
   }
 }
